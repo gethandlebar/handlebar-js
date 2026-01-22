c314a91e6b69158b415ffd5fdb14cf36e6c2de26
Tom Titcombe <tom@gethandlebar.com>
2026-01-22 17:25:08 +0000

    Include new conditions in type

--- packages/governance-schema/src/rules/v2/condition.ts
+++ packages/governance-schema/src/rules/v2/condition.ts
@@ -1,17 +1,20 @@
 import type { AndCondition, CustomFunctionCondition, EndUserTagCondition, ExecutionTimeCondition, MaxCallsCondition, NotCondition, OrCondition, SequenceCondition, ToolNameCondition, ToolTagCondition } from "../condition.types";
 import type { MetricWindowCondition } from "./metrics";
+import type { RequireSubjectCondition, SignalCondition } from "./signals";
 import type { TimeGateCondition } from "./time";
 
 export type RuleConditionV2 =
-	| ToolNameCondition
-	| ToolTagCondition
-	| EndUserTagCondition
-	| ExecutionTimeCondition
-	| SequenceCondition
-	| MaxCallsCondition
-	| CustomFunctionCondition
-	| MetricWindowCondition
-	| TimeGateCondition
-	| AndCondition
-	| OrCondition
-	| NotCondition;
+  | ToolNameCondition
+  | ToolTagCondition
+  | EndUserTagCondition
+  | ExecutionTimeCondition
+  | SequenceCondition
+  | MaxCallsCondition
+  | CustomFunctionCondition
+  | MetricWindowCondition
+  | TimeGateCondition
+  | RequireSubjectCondition
+  | SignalCondition
+  | AndCondition
+  | OrCondition
+  | NotCondition;
