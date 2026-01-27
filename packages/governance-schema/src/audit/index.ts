f5ccaf134e17e7c7348876c072d78fa4a5fe0b8d
Tom Titcombe <tom@gethandlebar.com>
2026-01-27 13:19:10 +0000

    Remove unecessary index

--- packages/governance-schema/src/audit/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./events";
-export * from "./events.base";
-export * from "./events.llm";
-export * from "./events.tools"
-export * from "./governance-actions";
-export * from "./run-metrics";
