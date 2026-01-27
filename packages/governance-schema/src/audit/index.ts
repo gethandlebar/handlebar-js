e6ebf92f759e6a9c0cef08162b5d7d8c50c80d1a
Tom Titcombe <tom@gethandlebar.com>
2026-01-27 10:05:02 +0000

    refactor: move tool events to own file

---
+++ packages/governance-schema/src/audit/index.ts
@@ -0,0 +1,6 @@
+export * from "./events";
+export * from "./events.base";
+export * from "./events.llm";
+export * from "./events.tools"
+export * from "./governance-actions";
+export * from "./run-metrics";
